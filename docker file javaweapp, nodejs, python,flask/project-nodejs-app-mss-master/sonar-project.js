9eda87382ab65e1a52b3becd21d160d4aba7d2ce
anjireddy - MSc embedded systems design <94956350+anjilinux@users.noreply.github.com>
2023-02-02 23:12:44 +0530

    Add files via upload

---
+++ docker file javaweapp, nodejs, python,flask/project-nodejs-app-mss-master/sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://13.233.255.130:9000',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
